462295039075eff844d31ecf37e95f7e65034caa
Piotr Smolinski <piotr.smolinski@confluent.io>
2021-07-16 19:40:15 +0200

    Added 'custom' SASL mechanism
    This addition opens a possibility to define multi-mechanism SASL ports

--- roles/confluent.variables/filter_plugins/filters.py
+++ roles/confluent.variables/filter_plugins/filters.py
@@ -29,13 +29,14 @@ class FilterModule(object):
             else 'SCRAM-SHA-256' if protocol.lower() == 'scram256' \
             else 'PLAIN' if protocol.upper() == 'PLAIN' \
             else 'OAUTHBEARER' if protocol.upper() == 'OAUTH' \
+            else 'custom' if protocol.lower() == 'custom' \
             else 'none'
         return normalized
 
     def kafka_protocol_normalized(self, sasl_protocol_normalized, ssl_enabled):
         # Joins a sasl mechanism and tls setting to return a kafka protocol
-        kafka_protocol = 'SASL_SSL' if ssl_enabled == True and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-512', 'SCRAM-SHA-256', 'OAUTHBEARER'] \
-            else 'SASL_PLAINTEXT' if ssl_enabled == False and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-512', 'SCRAM-SHA-256', 'OAUTHBEARER'] \
+        kafka_protocol = 'SASL_SSL' if ssl_enabled == True and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-512', 'SCRAM-SHA-256', 'OAUTHBEARER', 'custom'] \
+            else 'SASL_PLAINTEXT' if ssl_enabled == False and sasl_protocol_normalized in ['GSSAPI', 'PLAIN', 'SCRAM-SHA-512', 'SCRAM-SHA-256', 'OAUTHBEARER', 'custom'] \
             else 'SSL' if ssl_enabled == True and sasl_protocol_normalized == 'none' \
             else 'PLAINTEXT'
         return kafka_protocol
